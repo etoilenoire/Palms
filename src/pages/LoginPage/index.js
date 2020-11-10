39c21e7e756ef46cea8062dfa20be2ce44c0cdef
Mellelo <56165767+Mellelo@users.noreply.github.com>
2020-11-10 17:59:04 +0100

    First Pages (qui marchent pas)

---
+++ src/pages/LoginPage/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Button from '../../components/Button'
+import Input from '../../components/Input'
+import Lien from '../../components/Lien'
+import Title from '../../components/Title'
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+const LogIn = () => {
+    // return (
+    //     <Title/>
+    //     <div>
+    //         <Input/>
+    //         <Input/>
+    //         <Button/>
+    //         <Lien/>
+    //     </div>
+    // )
+}
+
+export default LogIn
