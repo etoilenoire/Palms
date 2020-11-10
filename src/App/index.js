e8d3f9e46676138d306c1a3dad047acb61223da8
Mellelo <56165767+Mellelo@users.noreply.github.com>
2020-11-10 17:46:00 +0100

    suppr Home

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,23 @@
 import styles from './App.module.css'
-import { Link } from 'react-router-dom';
-import HomePage from 'pages/HomePage'
+import { Link } from 'react-router-dom'
+import Button from '../components/Button'
+import Input from '../components/Input'
+import Title from '../components/Title';
+import Subtitle from '../components/SubTitle';
+import Lien from '../components/Lien';
+import CardArtist from '../components/CardArtist';
 
 
 function App() {
   return (
     <div>
-  
-      <HomePage/>
-      
+      <Title/>
+      <Subtitle/>
+      <Lien/>
+      <Button />
+      <Input />
+      <CardArtist/>
+      <CardArtist/>
     </div>
   );
 }
