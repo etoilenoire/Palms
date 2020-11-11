ee08b712247516f72b7ed30c58cd53b055045b7b
stephen nicolas <stephenbn@hotmail.fr>
2020-11-11 15:20:35 +0100

    ProfilePic header

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,8 @@ import styles from './App.module.css'
 import { Link } from 'react-router-dom'
 import LogIn from '../pages/LoginPage';
 import SignIn from '../pages/SignUpPage';
+import Header from '../components/Header';
+
 
 {/*import HomePage from '../pages/HomePage';*/}
 
@@ -9,6 +11,7 @@ import SignIn from '../pages/SignUpPage';
 function App() {
   return (
     <div>
+      <Header />
       <LogIn />
       <SignIn />
       {/*<HomePage />*/}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ProfilePic from '../ProfilePic'
 import Title from '../Title'
+import styles from './Header.module.css'
 
 const Header = () => {
     return (
