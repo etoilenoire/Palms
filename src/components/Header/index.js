3f152b098199f33722637e1ec16d604880567fd7
Mellelo <56165767+Mellelo@users.noreply.github.com>
2020-11-12 14:51:58 +0100

    Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,21 +5,19 @@ import {Navbar} from 'reactstrap'
 
 const Header = ({children}) => {
     return (
-<<<<<<< HEAD
-        <div className={styles.header}>
-            <div className={styles.title}><Title>{children}</Title></div>
-            <ProfilePic />
-            <div className={styles.rectangle}/>
-        </div>
-=======
 
         <Navbar>
             <Title>{children}</Title>
             <ProfilePic />
         </Navbar>
->>>>>>> 2fc90be59d631809baa855de8c3e516ba98a59e1
 
     )
 }
+
+{/* <div className={styles.header}>
+            <div className={styles.title}><Title>{children}</Title></div>
+            <ProfilePic />
+            <div className={styles.rectangle}/>
+        </div> */}
 
 export default Header
