1c9f047d946feb0dd478e3ff83c0f6fd06d92418
etoilenoire <pvelain@gmail.com>
2020-11-13 21:34:25 +0100

    debug for netlify

--- src/pages/StreamPage/index.js
+++ src/pages/StreamPage/index.js
@@ -3,8 +3,8 @@ import styles from './StreamPage.module.css'
 import Header from '../../components/Header'
 import Menu from '../../components/Menu'
 
-import CardConcert from '../../components/CardConcert'
-import CardArtist from '../../components/CardArtist'
+import CardConcert from '../../components/cardConcert'
+import CardArtist from '../../components/cardArtist'
 
 
 
