c8aee692fc019665a95adf1e1ecf438fec9994f9
etoilenoire <pvelain@gmail.com>
2020-11-13 14:34:47 +0100

    g

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -5,11 +5,7 @@ const UrlBanner = "https://cdn-www.konbini.com/fr/files/2013/06/Banniere.jpg"
 
 const Banner = () => {
     return (
-<<<<<<< HEAD
-        <img src={UrlBanner} alt="" className={styles.banner} />
-=======
         <img className={styles.banner} src={UrlBanner} alt=""/>
->>>>>>> 37b09138eada90fd7653c4da633a8e4d95087afa
     )
 }
 
