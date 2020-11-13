37b09138eada90fd7653c4da633a8e4d95087afa
Mellelo <56165767+Mellelo@users.noreply.github.com>
2020-11-13 14:31:54 +0100

    Modifs css Player et Carousel

--- src/App/index.js
+++ src/App/index.js
@@ -9,6 +9,7 @@ import HappenNowPage from '../pages/HappenNowPage';
 import HomePage from '../pages/Homepage';
 import ProfilPage from '../pages/ProfilPage';
 import Spotify from "../components/Spotify";
+import Player from "../components/Player";
 
 //import SpotifyProvider from "../components/SpotifyProvider";
 //import SpotifyWebPlayer from "react-spotify-web-playback/lib";
@@ -36,7 +37,7 @@ function App() {
         
         
        
-      <Player />
+      <Player/>
         
         
         
